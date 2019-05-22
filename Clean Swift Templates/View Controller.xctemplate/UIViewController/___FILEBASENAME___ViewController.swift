fec242084a91f68ea9cd40631a778534573b59a1
Rohit Funde <rrfunde@gmail.com>
2019-05-22 10:36:48 +0900

    added clean swift templates

---
+++ Clean Swift Templates/View Controller.xctemplate/UIViewController/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+
+import UIKit
+
+protocol ___VARIABLE_sceneName___DisplayLogic: class {
+    func displayError(viewModel: ___VARIABLE_sceneName___.Something.ViewModel)
+}
+
+class ___VARIABLE_sceneName___ViewController: UIViewController, ___VARIABLE_sceneName___DisplayLogic {
+    var interactor: ___VARIABLE_sceneName___BusinessLogic?
+    var router: (NSObjectProtocol & ___VARIABLE_sceneName___RoutingLogic & ___VARIABLE_sceneName___DataPassing)?
+    
+    
+    // MARK: Outlets
+    //@IBOutlet weak var nameTextField: UITextField!
+    
+    // MARK: Object lifecycle
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    // MARK: Setup
+    private func setup() {
+        let viewController = self
+        let interactor = ___VARIABLE_sceneName___Interactor()
+        let presenter = ___VARIABLE_sceneName___Presenter()
+        let router = ___VARIABLE_sceneName___Router()
+        viewController.interactor = interactor
+        viewController.router = router
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+        router.viewController = viewController
+        router.dataStore = interactor
+    }
+    
+    // MARK: View lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    
+    func setupUI() {
+        
+    }
+    
+    func displayError(viewModel: ___VARIABLE_sceneName___.Something.ViewModel) {
+
+    }
+}
